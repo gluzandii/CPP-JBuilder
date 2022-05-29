9150e8532f959945b4d28c8c968f8bffaacb426e
Sushant Nariangadu <nsushant2007@gmail.com>
2022-05-29 13:46:48 +0530

    Add include

--- src/tasks/build_task.hpp
+++ src/tasks/build_task.hpp
@@ -6,40 +6,77 @@
 
 #include "color/color.hpp"
 
-static void build_task(std::string cwd) {
+static void build_task(std::string cwd)
+{
     auto file = cwd + "/sjb.config.txt";
-    if (!std::filesystem::exists(file)) {
+    if (!std::filesystem::exists(file))
+    {
         color_println("No jbuild project found in the current directory.", RED);
         return;
     }
-    
+
     auto p = parse(file);
-    
+
     bool canExec = false;
     std::stringstream ss;
     ss << "javac ";
 
     std::string src = "", out = "";
-    if (p.find("src") != p.end() && p.find("out") != p.end()) {
+    if (p.find("src") != p.end() && p.find("out") != p.end())
+    {
         src = cwd + "/" + p.find("src")->second;
         out = cwd + "/" + p.find("out")->second;
 
-        ss << "-d " << out << " ";
+        ss << "-d \"" << out << "\" ";
 
-        for (const auto& i : std::filesystem::recursive_directory_iterator(src)) {
-            auto p = i.path();
-            if (p.extension() == ".java") {
-                if (!canExec) {
-                    canExec = true;
+        if (std::filesystem::exists(src) && std::filesystem::is_directory(src))
+        {
+            for (const auto &i : std::filesystem::recursive_directory_iterator(src))
+            {
+                auto p = i.path();
+                if (p.extension() == ".java")
+                {
+                    if (!canExec)
+                    {
+                        canExec = true;
+                    }
+                    ss << "\"" << p << "\" ";
                 }
-                ss << p << " ";
             }
         }
     }
 
-    if (canExec) {
+    if (p.find("include") != p.end())
+    {
+        auto include = cwd + "/" + p.find("include")->second;
+        auto s =
+#ifdef _WIN32
+            ";";
+#else
+            ":";
+#endif
+
+        if (std::filesystem::exists(include) && std::filesystem::is_directory(include))
+        {
+            for (const auto &i : std::filesystem::recursive_directory_iterator(include))
+            {
+                auto p = i.path();
+                if (p.extension() == ".jar")
+                {
+                    ss << "-cp \"" << p << s << p.parent_path() << "/*"
+                       << "\"";
+                    break;
+                }
+            }
+        }
+    }
+
+    if (canExec)
+    {
         std::system(std::string(ss.str()).c_str());
-    } else {
+    }
+    else
+    {
         color_println("No java files to compile in the " + src + " folder.", RED);
     }
 }
